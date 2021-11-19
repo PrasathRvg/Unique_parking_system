ba19d2a25f7e2bdb0728ba594cd433de1a914377
PrasathRvg <prasathrvg@gmail.com>
2021-11-19 19:45:11 +0530

    first commit

---
+++ UniqueParkingWebAPI/UniqueParkingWebAPI/Models/Booking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace UniqueParkingWebAPI.Models
+{
+    public partial class Booking
+    {
+        public Booking()
+        {
+            Payments = new HashSet<Payment>();
+        }
+
+        public int BookId { get; set; }
+        public string Id { get; set; }
+        public int Slotno { get; set; }
+        public DateTime Startdate { get; set; }
+        public DateTime? Enddate { get; set; }
+        public DateTime Starttime { get; set; }
+        public DateTime Endtime { get; set; }
+        public int Amount { get; set; }
+        public string CarNo { get; set; }
+
+        public virtual AspNetUser IdNavigation { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
---
+++ UniqueParkingWebAPI/UniqueParkingWebAPI/Models/Pass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace UniqueParkingWebAPI.Models
+{
+    public partial class Pass
+    {
+        public Pass()
+        {
+            Payments = new HashSet<Payment>();
+        }
+
+        public int PassId { get; set; }
+        public string Id { get; set; }
+        public int? Vechid { get; set; }
+        public DateTime Startdate { get; set; }
+        public string PassType { get; set; }
+        public int Cost { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int Slotno { get; set; }
+
+        public virtual AspNetUser IdNavigation { get; set; }
+        public virtual Vehicle Vech { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
